18364ef7f55413501412b2605b23f9ccc762b51f
BarreraJoel <joelmanuelbar@gmail.com>
2026-01-10 17:00:51 -0300

    docs(metrics): actualizar documentacion de metricas

--- backend/src/docs/components/responses/index.ts
+++ backend/src/docs/components/responses/index.ts
@@ -1,9 +1,11 @@
 import authResponse from "./auth";
 import errorResponse from "./error";
 import playerResponse from "./player";
+import metricsResponse from "./metrics";
 
 export default {
     ...errorResponse,
     ...authResponse,
     ...playerResponse,
+    ...metricsResponse,
 }
