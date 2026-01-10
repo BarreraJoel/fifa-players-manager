d231c743720915d9c96d5d0f224b2eb838252b5c
BarreraJoel <joelmanuelbar@gmail.com>
2026-01-10 03:52:31 -0300

    chore: implementar boton para volver

--- frontend/src/app/features/players/edit-player/edit-player.component.ts
+++ frontend/src/app/features/players/edit-player/edit-player.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { EditPlayerFormComponent } from "@/components/player/edit-player-form/edit-player-form.component";
+import { BackButtonComponent } from "@/components/common/back-button/back-button.component";
 
 @Component({
   selector: 'app-edit-player',
   imports: [
-    EditPlayerFormComponent
-  ],
+    EditPlayerFormComponent,
+    BackButtonComponent
+],
   templateUrl: './edit-player.component.html',
   styleUrl: './edit-player.component.css'
 })
